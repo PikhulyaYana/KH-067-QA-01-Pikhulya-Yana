cacb81bfd42c1d8f3ccf494098d637b412ca0506
PikhulyaYana <pikhulyayana@gmail.com>
2022-04-24 00:04:19 +0300

    Code doesn't run if input is not correct

--- Task3/src/ThirdClass.java
+++ Task3/src/ThirdClass.java
@@ -3,33 +3,35 @@ import java.util.Scanner;
 public class ThirdClass {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        double num1;
+        char operator;
+        double num2;
         System.out.println("Enter the first number");
-        double num1 = sc.nextDouble();
+        num1 = sc.nextDouble();
         System.out.println("Choose on of the possible operations: +, -, *, /, %");
-        char operator = sc.next().charAt(0);
+        operator = sc.next().charAt(0);
         if (operator == '+' || operator == '-' || operator == '*' || operator == '/' || operator == '%') {
             System.out.println("Enter the second number");
+            num2 = sc.nextDouble();
+
+            if (operator == '+') {
+                double res = num1 + num2;
+                System.out.println(res);
+            } else if (operator == '-') {
+                double res = num1 - num2;
+                System.out.println(res);
+            } else if (operator == '*') {
+                double res = num1 * num2;
+                System.out.println(res);
+            } else if (operator == '/') {
+                double res = num1 / num2;
+                System.out.println(res);
+            } else if (operator == '%') {
+                double res = num1 % num2;
+                System.out.println(res);
+            }
         } else
             System.out.println("Your input is not correct.");
-
-        double num2 = sc.nextDouble();
-
-        if (operator == '+') {
-            double res = num1 + num2;
-            System.out.println(res);
-        } else if (operator == '-') {
-            double res = num1 - num2;
-            System.out.println(res);
-        } else if (operator == '*') {
-            double res = num1 * num2;
-            System.out.println(res);
-        } else if (operator == '/') {
-            double res = num1 / num2;
-            System.out.println(res);
-        } else if (operator == '%') {
-            double res = num1 % num2;
-            System.out.println(res);
-        }
     }
 }
 
