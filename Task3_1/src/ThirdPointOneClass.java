c77e504c52400fa4ef7ba83ff9dbf91b4dd1c802
PikhulyaYana <pikhulyayana@gmail.com>
2022-04-23 23:50:51 +0300

    Code doesn't run if input is not correct

--- Task3_1/src/ThirdPointOneClass.java
+++ Task3_1/src/ThirdPointOneClass.java
@@ -3,33 +3,38 @@ import java.util.Scanner;
 public class ThirdPointOneClass {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        double num1;
+        double num2;
+        char operator;
         System.out.println("Enter the first number");
-        double num1 = sc.nextDouble();
+        num1 = sc.nextDouble();
         System.out.println("Choose on of the possible operations: +, -, *, /, %");
-        char operator = sc.next().charAt(0);
+        operator = sc.next().charAt(0);
         if (operator == '+' || operator == '-' || operator == '*' || operator == '/' || operator == '%') {
             System.out.println("Enter the second number");
+            num2 = sc.nextDouble();
+
+            switch (operator) {
+                case '+':
+                    System.out.println("Result = " + (num1 + num2));
+                    break;
+                case '-':
+                    System.out.println("Result = " + (num1 - num2));
+                    break;
+                case '*':
+                    System.out.println("Result = " + (num1 * num2));
+                    break;
+                case '/':
+                    System.out.println("Result = " + (num1 / num2));
+                    break;
+                case '%':
+                    System.out.println("Result = " + (num1 % num2));
+                    break;
+
+            }
         } else
             System.out.println("Your input is not correct.");
-        double num2 = sc.nextDouble();
-
-        switch (operator) {
-            case '+':
-                System.out.println("Result = " + (num1 + num2));
-                break;
-            case '-':
-                System.out.println("Result = " + (num1 - num2));
-                break;
-            case '*':
-                System.out.println("Result = " + (num1 * num2));
-                break;
-            case '/':
-                System.out.println("Result = " + (num1 / num2));
-                break;
-            case '%':
-                System.out.println("Result = " + (num1 % num2));
-                break;
-                
-        }
     }
 }
+
+
